ecb2b55f7b5576f219602e88e9ea47b75287bd1d
shefeel kv <shafeelshefi7777@gmail.com>
2026-07-21 22:53:22 +0530

    Serve static files dynamically on Vercel using Whitenoise finders

--- micro_copy_maker/settings.py
+++ micro_copy_maker/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_USE_FINDERS = True
+
 
 # Media files (User uploaded images and fonts)
 MEDIA_URL = '/media/'
